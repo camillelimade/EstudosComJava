68a323542e5f17b98cf08043b29e6b0cf55be77c
camille <camilledds0@gmail.com>
2026-01-15 11:28:42 -0300

    add: new class

--- src/MeuPrimeiroPrograma.java
+++ src/MeuPrimeiroPrograma.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class MeuPrimeiroPrograma {
+    public static void main (String[] args) {
+        System.out.println("chama");
+        int a = 0;
+        int b = 12;
+        while (a < b){
+            a++;
+            System.out.println("chama");
+        }
+    }
 }
