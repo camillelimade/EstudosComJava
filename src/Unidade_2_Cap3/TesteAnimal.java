c049ae2b55105af95df45c925dde43df44a695c7
camille <camilledds0@gmail.com>
2026-02-10 00:14:59 -0300

    task: finalizando questao 7  uni 2 cap 3

--- src/Unidade_2_Cap3/TesteAnimal.java
+++ src/Unidade_2_Cap3/TesteAnimal.java
@@ -1,4 +1,17 @@
 package Unidade_2_Cap3;
 
+import java.util.ArrayList;
+
 public class TesteAnimal {
+    public static void main(String[] args) {
+        ArrayList<Animal> animals = new ArrayList<>();
+        animals.add(new Gato("Nina", 12, "Viralata", "Tricolor"));
+        animals.add(new Gato("Fumaça", 4, "Gato Russo", "Cinza"));
+        animals.add(new Cachorro("Sandy", 7, "Pincher", "Preto"));
+        animals.add(new Cachorro("Tiana", 10, "Viralata", "Caramelo"));
+        for (Animal animal : animals) {
+            animal.fazerSom();
+        }
+
+    }
 }
