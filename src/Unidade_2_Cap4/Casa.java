91d938944c36642ff53e6157345c173b67228a8e
camille <camilledds0@gmail.com>
2026-02-14 21:14:35 -0300

    refactor: ajustes conceituais de métodos e arrays

--- src/Unidade_2_Cap4/Casa.java
+++ src/Unidade_2_Cap4/Casa.java
@@ -3,48 +3,22 @@ package Unidade_2_Cap4;
 import java.util.ArrayList;
 
 public class Casa {
-    ArrayList<Comodo> Comodos = new ArrayList<>();
-
-    public Casa() {
-
+    private ArrayList<Comodo> comodos = new ArrayList<>();
+    public void addComodo(String nome, double tamanho) {
+        comodos.add(new Comodo(tamanho, nome));
+        System.out.println("Cômodo " + nome + " adicionado!");
     }
-
-    public ArrayList<Comodo> getComodos() {
-        return Comodos;
-    }
-
-    public void setComodos(ArrayList<Comodo> comodos) {
-        Comodos = comodos;
-    }
-
-    public void AddComodo(double tamanho, String nome) {
-        Comodos.add(new Comodo(tamanho, nome));
-
-        System.out.println("Comodo " + nome + " adicionado com sucesso!");
-    }
-
-    public static void Divisor() {
-        System.out.println("-----------------------------------");
-    }
-
-    public static void ListarComodos(Casa casa) {
-        Divisor();
-        System.out.println("Listando comodos da casa solicitada: ");
-        Divisor();
-        for (Comodo comodo : casa.getComodos()) {
-            System.out.println(comodo.getNome());
+    public void listarComodos() {
+        System.out.println("---- CÔMODOS DA CASA ----");
+        for (Comodo c : comodos) {
+            System.out.println(c.getNome() + " - " + c.getTamanho() + " m²");
         }
     }
-
     public static void main(String[] args) {
-        Casa casa1 = new Casa();
-        casa1.AddComodo(100, "Mesa de jantar");
-        casa1.AddComodo(2.64, "Cama de Casal");
-        ListarComodos(casa1);
-
-        Casa casaCeciCamille = new Casa();
-        casaCeciCamille.AddComodo(120, "Escrivaninha");
-        casaCeciCamille.AddComodo(50, "Cadeira de Escritório");
-        ListarComodos(casaCeciCamille);
+        Casa casa = new Casa();
+        casa.addComodo("Sala", 20);
+        casa.addComodo("Quarto", 12);
+        casa.addComodo("Cozinha", 10);
+        casa.listarComodos();
     }
 }
--- src/Unidade_2_Cap4/Comodo.java
+++ src/Unidade_2_Cap4/Comodo.java
@@ -3,20 +3,16 @@ package Unidade_2_Cap4;
 public class Comodo {
     private String nome;
     private double tamanho;
-
     public Comodo(double tamanho, String nome) {
         this.tamanho = tamanho; // metros quadrados
         this.nome = nome;
     }
-
     public String getNome() {
         return nome;
     }
-
     public double getTamanho() {
         return tamanho;
     }
-
     public static void main(String[] args){
 
     }
