a23446027ca1e9c1f391032e7ca11d40c5e38e70
camille <138686617+camillelimade@users.noreply.github.com>
2026-08-04 15:17:11 -0300

    ADS: introdução ao conceito de classe, objeto, instância, e biblioteca Scanner

---
+++ src/ADS/AulaDois.java
@@ -0,0 +1,44 @@
+package ADS;
+
+import java.util.Scanner;
+
+public class AulaDois {
+    int idade;
+    String nome;
+    float altura;
+    float peso;
+
+    public AulaDois(int idade, String nome, float altura, float peso) {
+        this.idade = idade;
+        this.nome = nome;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno" + '\'' +
+                " Idade: " + idade + '\'' +
+                " Nome: '" + nome + '\'' +
+                " Altura: " + altura + '\'' +
+                " Peso: "+ peso;
+    }
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        
+        System.out.println("Digite sua idade:  ");
+        int idadeUser = leitor.nextInt();
+        leitor.nextLine();
+        System.out.println("Digite seu nome:  ");
+        String nomeUser = leitor.nextLine();
+
+
+        System.out.println("Digite sua altura:  ");
+        float alturaUser = leitor.nextFloat();
+
+        System.out.println("Digite seu peso:  ");
+        float pesoUser = leitor.nextFloat();
+        AulaDois camille = new AulaDois(idadeUser, nomeUser, alturaUser, pesoUser);
+    }
+}
