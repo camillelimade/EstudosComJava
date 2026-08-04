0d240c7eed8c4a24be129bdd5322eea1c30af101
Camille Lima <138686617+camillelimade@users.noreply.github.com>
2026-08-04 16:51:25 -0300

    feat: Problema 1, Atividade 1

---
+++ src/ADS/Aula02/Problema1.java
@@ -0,0 +1,29 @@
+package ADS.Aula02;
+
+import java.util.Scanner;
+
+public class Problema1 {
+    public static void main(String[] args) {
+        // criação da instancia da variavel de leitura out
+        Scanner out = new Scanner(System.in);
+        // variaveis de medidas do retangulo
+        float b, t;
+        // variaveis para obtenção do tamanho do corte via diferença
+        float ladoEsq, ladoDir;
+        System.out.println("Comprimento de b: ");
+        b = out.nextFloat();
+        System.out.println("Comprimento de t: ");
+        t = out.nextFloat();
+        // obtenção do tamanho de cada lado via diferença de comprimentos
+        ladoEsq = (b + t) * 70 / 2;
+        ladoDir = (160 * 70) - ladoEsq;
+        // condicional de comparação de valores que exibe cada resultado
+        if (ladoEsq > ladoDir){
+            System.out.println("Francisco (Lado Esquerdo maior)");
+        }else if (ladoEsq == ladoDir){
+            System.out.println("As areas são iguais");
+        }else {
+            System.out.println("Francisca (Lado Direito maior)");
+        }
+}
+}
