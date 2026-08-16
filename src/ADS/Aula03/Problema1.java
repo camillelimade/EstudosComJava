1b3fd9e0217cc9183e355008de28740f48d1122e
Camille Lima <138686617+camillelimade@users.noreply.github.com>
2026-08-16 12:30:02 -0300

    feat: resolução do problema 1 da aula 3 - ADS

---
+++ src/ADS/Aula03/Problema1.java
@@ -0,0 +1,19 @@
+package ADS.Aula03;
+
+public class Problema1 {
+    public static void main(String[] args) {
+        float preco = 5; // preço inicial do ingresso
+        int ingressos = 120; // quantidade inicial de ingressos
+        int despesas = 200; // despesas
+        while (preco >= 1.0){ // roda até o limite estabelecido pelo enunciado
+            float lucro = (preco * ingressos) - despesas; // calculo do lucro
+            System.out.println("Preço do ingresso: R$ " + preco); // exibe os preços atualizados a cada volta
+            System.out.println("Quantidade de ingressos: " + ingressos);
+            System.out.println("Lucro esperado: R$ " + lucro);
+            System.out.println("-----------------");
+            preco -= 0.50; //diminuindo o preço dos ingressos 50 centavos
+            ingressos += 26; // estimativa de aumento de compras em 26
+        }
+
+    }
+}
